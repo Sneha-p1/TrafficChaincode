5c16361082ddbccd898b168b8ca770b68f09a88e
Sneha-p1 <snehaparambadan@gmail.com>
2024-10-26 18:31:41 +0530

    chaincode

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const TrafficContract = require('./lib/traffic-contract');
+
+module.exports.TrafficContract = TrafficContract;
+
+module.exports.contracts = [ TrafficContract];
